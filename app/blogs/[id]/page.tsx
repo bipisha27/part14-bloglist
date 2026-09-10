9f0d3487646c11aa51007be0ac46073f74984375
Bipisha <paudelbipisha4@gmail.com>
2026-09-10 13:20:27 +0545

    add search by title using URL search params

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 import { getBlogById } from "../../services/blogs";
 import { likeBlogAction } from "../../actions/blogs";
 
+
 const BlogPage = async ({params}: {params: Promise<{id:string}> }) => {
   const {id} = await params
   const blog = getBlogById(Number(id))
--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,16 +1,34 @@
 import Link from 'next/link'
 import { getBlogs } from '../services/blogs'
 
-const Blogs = () => {
-  const blogs = getBlogs()
+const Blogs = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ filter?: string }>
+}) => {
+  const { filter } = await searchParams
+  const allBlogs = getBlogs()
+
+  const blogs = filter
+    ? allBlogs.filter((blog) =>
+        blog.title.toLowerCase().includes(filter.toLowerCase())
+      )
+    : allBlogs
+
   return (
     <div>
       <h2>Blogs</h2>
       <Link href="/blogs/new">Create a new blog</Link>
+
+      <form action="/blogs">
+        <input type="text" name="filter" defaultValue={filter} placeholder="Search by title ..." />
+        <button type="submit">Search</button>
+      </form>
+
       <ul>
-        {blogs.map(blog => (
+        {blogs.map((blog) => (
           <li key={blog.id}>
-            <Link href={`/blogs/${blog.id}`}>{blog.title}</Link> by {blog.author}
+            <Link href={`/blogs/${blog.id}`}>{blog.title}</Link> by {blog.author}, {blog.likes} likes
             <br />
             <a href={blog.url}>{blog.url}</a>
           </li>
